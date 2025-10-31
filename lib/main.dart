ddb24169d05862f7cfe61bb66234c1086be56cb9
Niruban-A <nirubanchipriyan1453@gmail.com>
2025-10-31 12:19:24 +0530

    animated widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home:Imageanimation() ,));
+void main() {
+  runApp(MaterialApp(home: Imageanimation()));
 }
+
+class ReusableAnimation extends AnimatedWidget {
+  const ReusableAnimation({super.key, required Animation<double> animation})
+    : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: SizedBox(
+        height: animation.value,
+        width: animation.value,
+        child: Image.asset("assets/img.png"),
+      ),
+    );
+  }
+}
+
 class Imageanimation extends StatefulWidget {
   const Imageanimation({super.key});
 
@@ -10,33 +28,36 @@ class Imageanimation extends StatefulWidget {
   State<Imageanimation> createState() => _ImageanimationState();
 }
 
-class _ImageanimationState extends State<Imageanimation> with SingleTickerProviderStateMixin {
+class _ImageanimationState extends State<Imageanimation>
+    with SingleTickerProviderStateMixin {
   late Animation animation;
   late AnimationController animationController;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    animationController=AnimationController(vsync: this,duration: Duration(seconds: 5));
-    animation= Tween<double>(begin:0 ,end: 300).animate(animationController)
-    .. addListener((){
-      setState(() {
-        //
-      });
-    }
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
     );
-     animationController.forward();
+    animation = Tween<double>(begin: 0, end: 300).animate(animationController)
+      ..addStatusListener((status) {
+        if (status==AnimationStatus.completed) {
+          animationController.reverse();
+        } else if(status==AnimationStatus.dismissed) {
+          animationController.forward();
+        }
+      })
+      ..addStatusListener((status) {
+        print(status);
+      },);
+      animationController.forward();
+      
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: SizedBox(
-          
-          height: animation.value,
-          width: animation.value,
-          child: Image.asset("assets/img.png"),
-        ),
-      ),
+    return Scaffold(
+      body: ReusableAnimation(animation: animation as Animation<double>),
     );
   }
 }
